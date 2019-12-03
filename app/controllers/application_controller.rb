2784b3cca483212ff863fff174a09244b41b0202
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-03 11:29:12 +0900

    サイドバーにジャンル一覧表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :genre
+  def genre
+    @genres = Genre.all
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
