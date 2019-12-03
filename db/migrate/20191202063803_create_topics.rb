9a3630b7ef6f30d5b28cfbb6c3bcdf29ab9e8a0c
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-03 12:03:53 +0900

    修正　:〇〇_id

--- db/migrate/20191202063803_create_topics.rb
+++ db/migrate/20191202063803_create_topics.rb
@@ -1,8 +1,8 @@
 class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
-      t.integer :genre
-      t.integer :user
+      t.integer :genre_id
+      t.integer :user_id
       t.string :name
 
       t.timestamps
