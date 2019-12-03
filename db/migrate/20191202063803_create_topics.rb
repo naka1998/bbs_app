8bbd2694dcf06c2c815ed8da6fce3b947e7c60f4
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-03 11:58:20 +0900

    外部キー制約見直し

--- db/migrate/20191202063803_create_topics.rb
+++ db/migrate/20191202063803_create_topics.rb
@@ -2,7 +2,7 @@ class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
       t.integer :genre
-      t.references :user, foreign_key: true
+      t.integer :user
       t.string :name
 
       t.timestamps
--- db/migrate/20191202063809_create_posts.rb
+++ db/migrate/20191202063809_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.references :topic, foreign_key: true
-      t.references :user, foreign_key: true
+      t.integer :user
       t.integer :reply_post_id
       t.string :body
 
