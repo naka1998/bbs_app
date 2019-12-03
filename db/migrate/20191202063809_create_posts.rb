1bf569737e29c73ea0e51673c7a89a3eab845625
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-03 11:32:17 +0900

    テーブル設計変更

--- db/migrate/20191202063809_create_posts.rb
+++ db/migrate/20191202063809_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.references :topic, foreign_key: true
       t.references :user, foreign_key: true
-      t.string :poster_name
+      t.integer :reply_post_id
       t.string :body
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -34,7 +36,15 @@ end
   Post.create!(
     topic_id: i / 4 + 1,
     user_id: i + 2,
-    poster_name: "名無し#{i + 1}",
+    reply_post_id: "",
     body: "本文#{i + 1}"
+  )
+end
+20.times do |i|
+  Post.create!(
+    topic_id: i / 4 + 1,
+    user_id: i + 2,
+    reply_post_id: i + 1,
+    body: "返信本文#{i + 1}"
   )
 end
