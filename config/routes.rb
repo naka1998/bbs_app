5db1f7c7c940f2caae39700073f729ec4dae1cab
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-12-03 11:32:27 +0900

    作成　ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'topics#index'
+  resources :genres, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :topics, only: [:index, :show, :new, :create, :edit, :update, :destroy], shallow: true do
+    resources :posts, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
